90bc776dc6de5c229f057b8595cbbc26941287cc
Mimamed <54266354+Mimamed@users.noreply.github.com>
2019-09-01 17:33:41 +0200

    s

--- src/mimo/start.java
+++ src/mimo/start.java
@@ -1,15 +1,23 @@
 package mimo;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class start 
 {
 	static JFrame fönster = new JFrame();
 	static startMeny meny = new startMeny();
+	static JPanel gamlaPanelen = new JPanel();
 	
 	public static void main(String[] Args)
 	{
 		fönster.add(meny);
+		gamlaPanelen = meny;
 		
+	}
+	
+	public static void Byta(JPanel panel)
+	{
+		fönster.remove(gamlaPanelen);
 	}
 }
