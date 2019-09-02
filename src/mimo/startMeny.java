ce2b7ab00fa9c7d0c486712bbc816c49d03de658
ba-dum-ts <carlos.leyton@elev.cybergymnasiet.se>
2019-09-02 03:12:04 +0200

    lagt till bild

--- src/mimo/startMeny.java
+++ src/mimo/startMeny.java
@@ -1,22 +1,28 @@
 package mimo;
 
+import java.awt.Image;
+
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JFrame;
 
 public class startMeny extends JPanel
 {
 	static private JPanel pan = new JPanel(); // förkortning för panel
-	static private JButton but = new JButton(); // förkortning för button
+	static private JLabel lab = new JLabel(); // förkortning för label
 	
 	startMeny()
 	{
 		gui();
 	}
 	
-	public void gui()
+	void gui()
 	{
-		but.setIcon(new ImageIcon("C:\\Users\\Carlo\\Desktop\\adobe illustrator projects\\startMeny.ai"));
+		Image startMenu = new ImageIcon(this.getClass().getResource("/startMeny.pdf")).getImage();
+		lab.setIcon(new ImageIcon(startMenu));
+		pan.add(lab);
+		add(pan);
+		
+		validate();
 	}
 }
