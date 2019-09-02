bca0c65044d2aa794688284c55f7b82fbb377f3e
ba-dum-ts <carlos.leyton@elev.cybergymnasiet.se>
2019-09-02 02:20:41 +0200

    la till bild till panel

--- src/mimo/startMeny.java
+++ src/mimo/startMeny.java
@@ -1,20 +1,22 @@
 package mimo;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JFrame;
 
 public class startMeny extends JPanel
 {
-	/*static JFrame win = new JFrame();
-	static JButton button = new JButton();
-	static JPanel pan = new JPanel();
+	static private JPanel pan = new JPanel(); // förkortning för panel
+	static private JButton but = new JButton(); // förkortning för button
 	
 	startMeny()
 	{
-		win.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		win.setUndecorated(true);
-		win.setVisible(true);
-		
-	}*/
+		gui();
+	}
+	
+	public void gui()
+	{
+		but.setIcon(new ImageIcon("C:\\Users\\Carlo\\Desktop\\adobe illustrator projects\\startMeny.ai"));
+	}
 }
