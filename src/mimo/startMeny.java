829809a9a175a4124b97c5a78dcd86bd9cb933b2
Mimamed <54266354+Mimamed@users.noreply.github.com>
2019-09-03 14:31:06 +0200

    clejton carry

--- src/mimo/startMeny.java
+++ src/mimo/startMeny.java
@@ -1,6 +1,11 @@
 package mimo;
 
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -9,19 +14,35 @@ public class startMeny extends JPanel
 {
 	static private JPanel pan = new JPanel(); // förkortning för panel
 	static private JLabel lab = new JLabel(); // förkortning för label
+	static  BufferedImage pic; // förkortning för picture
+	static private File fil = new File("images\\Bild.jpg"); // förkortning för file
 	
 	startMeny()
 	{
 		gui(); // kallar på gui metod
+		try
+		{
+			pic = ImageIO.read(fil);
+		}
+		
+		catch(Exception e)
+		{
+			System.out.print("ndmkaj");
+		}
 	} //lol
+	
+	public void paintComponent(Graphics g)
+	{
+		g.drawImage(pic, 0, 0, this.getWidth(), this.getHeight(), null);
+	}
 	
 	public void gui()
 	{
-		Image startMenu = new ImageIcon(this.getClass().getResource("/startMeny.pdf")).getImage(); // skapar en väg till source folder för att hämta "startMeny" bild
-		lab.setIcon(new ImageIcon(startMenu)); // placerar bilden i min label
+		
+		/*lab.setIcon(new ImageIcon(/startMeny.pdf)); // placerar bilden i min label
 		pan.add(lab); // placerar min label i min panel
 		add(pan); // placerar min panel i min frame
 		
 		validate(); // uppdaterar bilden
-	}
+*/	}
 }
