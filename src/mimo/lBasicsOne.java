a3d3dd8fdc8acc58766f2c3edc4bc3b1166b395e
Erik <erik.sevelin@elev.cybergymnasiet.se>
2019-12-10 13:58:58 +0100

    fixat basics one, tillagd bild och fixar

--- src/mimo/lBasicsOne.java
+++ src/mimo/lBasicsOne.java
@@ -1,7 +1,122 @@
 package mimo;
 
-import java.util.Scanner;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
-public class lBasicsOne {
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class lBasicsOne extends JPanel {
+	static BufferedImage bakgrund;
+	static JButton lessonButton = new JButton(), flashcardButton = new JButton(), readingButton = new JButton(), menyButton = new JButton(), startButton = new JButton();
 	
+	lBasicsOne()
+	{
+		try
+		{
+			bakgrund = ImageIO.read(start.filer[9]);
+		}catch(Exception e)
+		{
+			System.out.print("fel men uppladning30");
+		}
+		
+		this.setLayout(null);
+		
+		//addar grejer
+		
+		this.add(lessonButton);
+		this.add(flashcardButton);
+		this.add(readingButton);
+		this.add(menyButton);
+		this.add(startButton);
+		
+		//sätter upp knapparna
+		
+		lessonButton.setBounds((int) Math.round(127 * start.widthSize), (int) Math.round(12 * start.heightSize), (int) Math.round(371 * start.widthSize), (int) Math.round(76 * start.heightSize));
+		lessonButton.addActionListener(new lessonButton());
+		lessonButton.setContentAreaFilled(start.synligaKnappar);
+		lessonButton.setBorderPainted(start.synligaKnappar);
+		lessonButton.setFocusPainted(start.synligaKnappar);
+		lessonButton.setFont(new Font("comic sans ms", Font.BOLD, 30));
+		lessonButton.setText("Lessons");
+
+		flashcardButton.setBounds((int) Math.round(499 * start.widthSize), (int) Math.round(12 * start.heightSize), (int) Math.round(370 * start.widthSize), (int) Math.round(76 * start.heightSize));
+		flashcardButton.addActionListener(new flashcardButton());
+		flashcardButton.setContentAreaFilled(start.synligaKnappar);
+		flashcardButton.setBorderPainted(start.synligaKnappar);
+		flashcardButton.setFocusPainted(start.synligaKnappar);
+		flashcardButton.setFont(new Font("comic sans ms", Font.BOLD, 30));
+		flashcardButton.setText("Flashcards");
+		
+		readingButton.setBounds((int) Math.round(870 * start.widthSize), (int) Math.round(12 * start.heightSize), (int) Math.round(370 * start.widthSize), (int) Math.round(76 * start.heightSize));
+		readingButton.addActionListener(new readingButton());
+		readingButton.setContentAreaFilled(start.synligaKnappar);
+		readingButton.setBorderPainted(start.synligaKnappar);
+		readingButton.setFocusPainted(start.synligaKnappar);
+		readingButton.setFont(new Font("comic sans ms", Font.BOLD, 30));
+		readingButton.setText("Reading");
+		
+		menyButton.setBounds((int) Math.round(10 * start.widthSize), (int) Math.round(10 * start.heightSize), (int) Math.round(80 * start.widthSize), (int) Math.round(80 * start.heightSize));
+		menyButton.addActionListener(new menyButton());
+		menyButton.setContentAreaFilled(start.synligaKnappar);
+		menyButton.setBorderPainted(start.synligaKnappar);
+		menyButton.setFocusPainted(start.synligaKnappar);
+		
+		startButton.setBounds((int) Math.round(652 * start.widthSize), (int) Math.round(241 * start.heightSize), (int) Math.round(87 * start.widthSize), (int) Math.round(56 * start.heightSize));
+		startButton.addActionListener(new startButtonAct());
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		g.drawImage(bakgrund, 0, 0, this.getWidth(), this.getHeight(), null);
+	}
+	
+	static class lessonButton implements ActionListener
+	{
+		
+		public void actionPerformed(ActionEvent e)
+		{
+			start.Byta(start.lektionTräd);
+		}
+	}
+	
+	static class flashcardButton implements ActionListener
+	{
+		
+		public void actionPerformed(ActionEvent e)
+		{
+			start.Byta(start.flashkort);
+		}
+	}
+	
+	static class readingButton implements ActionListener
+	{
+		
+		public void actionPerformed(ActionEvent e)
+		{
+			start.Byta(start.readingMenyn);
+		}
+	}
+	
+	static class menyButton implements ActionListener
+	{
+		
+		public void actionPerformed(ActionEvent e)
+		{
+			start.Byta(start.meny);
+		}
+	}
+	
+	static class startButtonAct implements ActionListener
+	{
+		
+		public void actionPerformed(ActionEvent e)
+		{
+			start.Byta(start.meny);
+		}
+	}
 }
--- src/mimo/start.java
+++ src/mimo/start.java
@@ -18,7 +18,7 @@ public class start
 	static boolean synligaKnappar = false;
 	static int[] ljusGrå = {200, 200, 200};
 	static double widthSize = Toolkit.getDefaultToolkit().getScreenSize().getWidth()/1366, heightSize = Toolkit.getDefaultToolkit().getScreenSize().getHeight()/768;
-	static URL[] filer = { start.class.getResource("/Bilder/startMenu.jpg"), start.class.getResource("/Bilder/tabMenu.jpg"), start.class.getResource("/Bilder/flashcardMenu.jpg"), start.class.getResource("/Bilder/flashcardGame.jpg"), start.class.getResource("/Bilder/readingMenu.jpg"), start.class.getResource("/Bilder/flashcardList.jpg"), start.class.getResource("/Bilder/readingGame.jpg"), start.class.getResource("/Bilder/settings.jpg"), start.class.getResource("/Bilder/lessonMenu.jpg")};
+	static URL[] filer = { start.class.getResource("/Bilder/startMenu.jpg"), start.class.getResource("/Bilder/tabMenu.jpg"), start.class.getResource("/Bilder/flashcardMenu.jpg"), start.class.getResource("/Bilder/flashcardGame.jpg"), start.class.getResource("/Bilder/readingMenu.jpg"), start.class.getResource("/Bilder/flashcardList.jpg"), start.class.getResource("/Bilder/readingGame.jpg"), start.class.getResource("/Bilder/settings.jpg"), start.class.getResource("/Bilder/lessonMenu.jpg"), start.class.getResource("/Bilder/lessonGameBasics.jpg")};
 	//static URL[] filerColorBlindMode = {start.class.getResource("/Bilder/startMenuCB.jpg"), start.class.getResource("/Bilder/tabMenuCB.jpg"), start.class.getResource("/Bilder/flashcardMenuCB.jpg"), start.class.getResource("/Bilder/flashcardGameCB.jpg"), start.class.getResource("/Bilder/readingMenuCB.jpg"), start.class.getResource("/Bilder/flashcardListCB.jpg"), start.class.getResource("/Bilder/readingGameCB.jpg"), start.class.getResource("/Bilder/settingsCB.jpg"), start.class.getResource("/Bilder/lessonMenuCB.jpg")};
 	static Formatter filskapare;
 	static List<ArrayList<String>> wordList = new ArrayList<ArrayList<String>>();
@@ -36,6 +36,7 @@ public class start
 	static readingGame readingSpel = new readingGame();
 	static settingsMenu inställningar = new settingsMenu();
 	static lessonTree lektionTräd = new lessonTree();
+	static lBasicsOne lektionGrunder = new lBasicsOne();
 	
 	
 	public static void main(String[] Args)
