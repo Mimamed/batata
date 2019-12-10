078540fc6d4da838a181cf535f7247d7711a6088
Erik <erik.sevelin@elev.cybergymnasiet.se>
2019-12-10 16:39:51 +0100

    klasser för 2 lektioner

--- src/mimo/lBasicsOneL1.java
+++ src/mimo/lBasicsOneL1.java
@@ -10,18 +10,18 @@ import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class lBasicsOne extends JPanel {
+public class lBasicsOneL1 extends JPanel {
 	static BufferedImage bakgrund;
-	static JButton lessonButton = new JButton(), flashcardButton = new JButton(), readingButton = new JButton(), menyButton = new JButton(), startL1Button = new JButton(), startL2Button = new JButton();
+	static JButton lessonButton = new JButton(), flashcardButton = new JButton(), readingButton = new JButton(), menyButton = new JButton();
 	
-	lBasicsOne()
+	lBasicsOneL1 ()
 	{
 		try
 		{
-			bakgrund = ImageIO.read(start.filer[9]);
+			bakgrund = ImageIO.read(start.filer[10]);
 		}catch(Exception e)
 		{
-			System.out.print("fel men uppladning30");
+			System.out.print("fel men uppladning0");
 		}
 		
 		this.setLayout(null);
@@ -32,8 +32,6 @@ public class lBasicsOne extends JPanel {
 		this.add(flashcardButton);
 		this.add(readingButton);
 		this.add(menyButton);
-		this.add(startL1Button);
-		this.add(startL2Button);
 		
 		//sätter upp knapparna
 		
@@ -66,22 +64,6 @@ public class lBasicsOne extends JPanel {
 		menyButton.setContentAreaFilled(start.synligaKnappar);
 		menyButton.setBorderPainted(start.synligaKnappar);
 		menyButton.setFocusPainted(start.synligaKnappar);
-		
-		startL1Button.setBounds((int) Math.round(652 * start.widthSize), (int) Math.round(241 * start.heightSize), (int) Math.round(87 * start.widthSize), (int) Math.round(56 * start.heightSize));
-		startL1Button.addActionListener(new startL1ButtonAct());
-		startL1Button.setContentAreaFilled(start.synligaKnappar);
-		startL1Button.setBorderPainted(start.synligaKnappar);
-		startL1Button.setFocusPainted(start.synligaKnappar);
-		startL1Button.setFont(new Font("comic sans ms", Font.BOLD, 20));
-		startL1Button.setText("Start");
-		
-		startL2Button.setBounds((int) Math.round(652 * start.widthSize), (int) Math.round(430 * start.heightSize), (int) Math.round(87 * start.widthSize), (int) Math.round(56 * start.heightSize));
-		startL2Button.addActionListener(new startL2ButtonAct());
-		startL2Button.setContentAreaFilled(start.synligaKnappar);
-		startL2Button.setBorderPainted(start.synligaKnappar);
-		startL2Button.setFocusPainted(start.synligaKnappar);
-		startL2Button.setFont(new Font("comic sans ms", Font.BOLD, 20));
-		startL2Button.setText("Start");
 	}
 	
 	public void paintComponent(Graphics g)
@@ -117,24 +99,6 @@ public class lBasicsOne extends JPanel {
 	}
 	
 	static class menyButton implements ActionListener
-	{
-		
-		public void actionPerformed(ActionEvent e)
-		{
-			start.Byta(start.meny);
-		}
-	}
-	
-	static class startL1ButtonAct implements ActionListener
-	{
-		
-		public void actionPerformed(ActionEvent e)
-		{
-			start.Byta(start.lektionGrunderL1);
-		}
-	}
-	
-	static class startL2ButtonAct implements ActionListener
 	{
 		
 		public void actionPerformed(ActionEvent e)
