512a313f4283a3f9e830fda26f06c0930585778e
Erik <erik.sevelin@elev.cybergymnasiet.se>
2019-12-12 08:20:18 +0100

    Alla värden för första lektionen inne

--- src/mimo/lBasicsOneL1.java
+++ src/mimo/lBasicsOneL1.java
@@ -8,18 +8,25 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+/*
+BEHÖVER: SÄTT ATT SVARA
+ */
+
 public class lBasicsOneL1 extends JPanel {
 	static BufferedImage bakgrund;
-	static JButton lessonButton = new JButton(), flashcardButton = new JButton(), readingButton = new JButton(), menyButton = new JButton();
+	static JButton lessonButton = new JButton(), flashcardButton = new JButton(), readingButton = new JButton(), menyButton = new JButton(), answerButton = new JButton();
 	static JTextArea textArea = new JTextArea();
-	static List<String> lessonSentences = new ArrayList(), sentencesTranslated = new ArrayList();
+	static List<SentenceList> lessonSentences = new ArrayList();
 	public static int whichSentence;
+	static Random randomNumber = new Random();
+	static int sentenceNumber;
 	
 	lBasicsOneL1 ()
 	{
@@ -34,30 +41,30 @@ public class lBasicsOneL1 extends JPanel {
 		this.setLayout(null);
 		
 		//addar grejer
-		
-		lessonSentences.add("Jag är en man");
-		lessonSentences.add("Du är en flicka");
-		lessonSentences.add("Han är en pojke");
-		lessonSentences.add("Hon är en kvinna");
-		lessonSentences.add("Jag är en kvinna");
-		lessonSentences.add("Du är en pojke");
-		lessonSentences.add("Han är en man");
-		lessonSentences.add("Hon är en flicka");
-		
-		sentencesTranslated.add("I am a man");
-		sentencesTranslated.add("You are a girl");
-		sentencesTranslated.add("He is a boy");
-		sentencesTranslated.add("She is a woman");
-		sentencesTranslated.add("I am a woman");
-		sentencesTranslated.add("You are a boy");
-		sentencesTranslated.add("He is a man");
-		sentencesTranslated.add("She is a girl");
+		//DETTA KAN BLI MYCKET BÄTTRE
+		SentenceList sentenceInfo = new SentenceList("Jag är en man", "I am a man", 0, 0, false);
+		lessonSentences.add(sentenceInfo);
+		sentenceInfo = new SentenceList("Du är en flicka", "You are a girl", 0, 0, false);
+		lessonSentences.add(sentenceInfo);
+		sentenceInfo = new SentenceList("Han är en pojke", "He is a boy", 0, 0, false);
+		lessonSentences.add(sentenceInfo);
+		sentenceInfo = new SentenceList("Hon är en kvinna", "She is a woman", 0, 0, false);
+		lessonSentences.add(sentenceInfo);
+		sentenceInfo = new SentenceList("Jag är en kvinna", "I am a woman", 0, 0, false);
+		lessonSentences.add(sentenceInfo);
+		sentenceInfo = new SentenceList("Du är en pojke", "You are a boy", 0, 0, false);
+		lessonSentences.add(sentenceInfo);
+		sentenceInfo = new SentenceList("Han är en man", "He is a man", 0, 0, false);
+		lessonSentences.add(sentenceInfo);
+		sentenceInfo = new SentenceList("Hon är en flicka", "She is a girl", 0, 0, false);
+		lessonSentences.add(sentenceInfo);
 		
 		this.add(lessonButton);
 		this.add(flashcardButton);
 		this.add(readingButton);
 		this.add(menyButton);
 		this.add(textArea);
+		this.add(answerButton);
 		
 		//sätter upp knapparna
 		
@@ -92,18 +99,31 @@ public class lBasicsOneL1 extends JPanel {
 		menyButton.setFocusPainted(start.synligaKnappar);
 		
 		textArea.setBounds((int) Math.round(200 * start.widthSize), (int) Math.round(155 * start.heightSize), (int) Math.round(965 * start.widthSize), (int) Math.round(500 * start.heightSize));
-		textArea.setFont(new Font("comic sans ms", Font.BOLD, 22));
+		textArea.setFont(new Font("comic sans ms", Font.BOLD, 30));
 		textArea.setLineWrap(true);
 		textArea.setWrapStyleWord(true);
 		textArea.setOpaque(false);
 		textArea.setForeground(Color.BLACK);
 		textArea.setEditable(false);
-		textArea.setText("Translate into English: " + lessonSentences.get(0));
+		textArea.setText("Test");
+		
+		answerButton.setBounds((int) Math.round(900 * start.widthSize), (int) Math.round(600 * start.heightSize), (int) Math.round(295 * start.widthSize), (int) Math.round(107 * start.heightSize));
+		answerButton.addActionListener(new answerButtonAct());
+		answerButton.setFont(new Font("comic sans ms", Font.BOLD, 30));
+		answerButton.setText("Answer");
+		
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		g.drawImage(bakgrund, 0, 0, this.getWidth(), this.getHeight(), null);
+	}
+	
+	public static void l1RandomSentence()
+	{
+		//FOR LOOP SOM RÄKNAR correctAnswer = false;
+		sentenceNumber = randomNumber.nextInt(8);
+		textArea.setText("Translate into English: " + lessonSentences.get(sentenceNumber).getSentence());
 	}
 	
 	static class lessonButton implements ActionListener
@@ -141,4 +161,50 @@ public class lBasicsOneL1 extends JPanel {
 			start.Byta(start.meny);
 		}
 	}
+	
+	static class answerButtonAct implements ActionListener
+	{
+		
+		public void actionPerformed(ActionEvent e)
+		{
+			lessonSentences.get(sentenceNumber).setCorrectAnswer(true);
+		}
+	}
+}
+
+class SentenceList
+{
+	private String sentence;
+    private String translation;
+    private int points;
+    private int tries;
+    private boolean correctAnswer;
+    
+    public SentenceList(String sentence, String translation, int points, int tries, boolean correctAnswer)
+    {
+    	this.sentence = sentence;
+    	this.translation = translation;
+    	this.points = points;
+    	this.tries = tries;
+    	this.correctAnswer = correctAnswer;
+    }
+    
+    public void setPoints(int points)
+    {
+    	this.points = points;
+    }
+    public void setTries(int tries)
+    {
+    	this.tries = tries;
+    }
+    public void setCorrectAnswer(boolean correctAnswer)
+    {
+    	this.correctAnswer = correctAnswer;
+    }
+    
+    public String getSentence() { return sentence; }
+    public String getTranslation() { return translation; }
+    public int getPoints() { return points; }
+    public int getTries() { return tries; }
+    public boolean getCorrectAnswer() { return correctAnswer; }
 }
